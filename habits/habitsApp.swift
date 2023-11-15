1e216ba2b1733c75a20a9196561f81578dd2047f
p-yates <148437369+p-yates@users.noreply.github.com>
2023-11-15 10:56:01 +0100

    Initial Commit

---
+++ habits/habitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  habitsApp.swift
+//  habits
+//
+//  Created by Paul Y on 2023-11-15.
+//
+
+import SwiftUI
+
+@main
+struct habitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

f149cc1a0010a8f8a0bbd5f97af049564c5a06ba
p-yates <148437369+p-yates@users.noreply.github.com>
2023-11-16 12:16:54 +0100

    add simple HabitDetailView and update ContentView main page to link to the HabitDetialView - edit button on detail view links to EditHabitView

---
+++ habits/Views/HabitDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  HabitDetailView.swift
+//  habits
+//
+//  Created by Paul Y on 2023-11-16.
+//
+
+import SwiftUI
+
+struct HabitDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HabitDetailView()
+}
